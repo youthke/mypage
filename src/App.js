5b95f20efe0fbb111dd781b02547f4cf4b6510ac
youthke <47767005+youthke@users.noreply.github.com>
2020-06-25 00:48:23 +0900

    add: routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom'
+
 import { Header } from "./component/Header";
 import {Layout} from "antd";
 import {About} from "./component/About";
 import "./index.css"
+import {Violet} from "./component/products/Violet";
 
 
 function App() {
   return (
       <Layout>
-      <Header/>
-      <About/>
+          <Header/>
+          <Router>
+              <Switch>
+                  <Route path={"/mypage"} component={About} exact/>
+                  <Route path={"/mypage/products/violet"} component={Violet}/>
+              </Switch>
+          </Router>
       </Layout>
+
   );
 }
 
