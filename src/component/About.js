910280938c9f0fc9e1f878b908daab597b313077
youthke <47767005+youthke@users.noreply.github.com>
2020-06-25 02:16:01 +0900

    add introduction

--- src/component/About.js
+++ src/component/About.js
@@ -26,11 +26,33 @@ export const About = () =>{
                     <Row type="flex" justify="center">
                         <Col span={12}>
                             <Card>
-                                <Title level={3} style={{ marginLeft: 10, marginBottom: 25 }}>
-                                    名前
+                                <Title level={5} style={{ marginLeft: 10, marginBottom: 25 }}>
+                                    自己紹介
+                                </Title>
+                                <Title level={2} style={{ marginLeft: 10, marginBottom: 25 }}>
+                                    基本情報
+                                </Title>
+                                <p>名前: 浅野 耀介</p>
+                                <p>年齢: 22</p>
+                                <p>趣味: ぷよぷよ, 散歩</p>
+                                <Title level={2} style={{ marginLeft: 10, marginBottom: 25 }}>
+                                    技術
+                                </Title>
+                                <ul>
+                                    <li>Go</li>
+                                    <li>React.js</li>
+                                    <li>Next.js</li>
+                                    <li>TypeScript</li>
+                                </ul>
+                                <Title level={2} style={{ marginLeft: 10, marginBottom: 25 }}>
+                                    紹介文
                                 </Title>
                                 <div>
-                                    浅野　耀介
+                                    学部2年の時にウェブアプリケーションを作ろうと大学の履修単位の管理するアプリをRailsで作成.
+                                    学部3年になって, 研究室に配属され, 春の課題として, 研究室の図書を管理するアプリをグループで開発. 秋学期では, 授業内で行われるプロジェクトに参加して航空管理システムをチームで開発した.　春の課題と授業でのプロジェクトはjsp servletを使用しました.
+                                    また,　1年ほどアジャイル開発を経験できるインターンに参加させていただき, Go, Reactの技術も持っています.
+                                    現在は, 研究室で後輩にウェブアプリケーション制作のゼミをしたり, 卒業生の先輩と一緒に
+                                    golangとNext.jsを使って開発したりしています.
                                 </div>
                             </Card>
                         </Col>
